a99f4312ac056fc16bdb930d615b339d9b18380a
Gerson Azevedo <gersonazgo@gmail.com>
2023-12-10 19:10:31 -0300

    Add biblio record model

--- app/models/biblio_record.rb
+++ app/models/biblio_record.rb
@@ -4,7 +4,6 @@ class BiblioRecord < ApplicationRecord
   before_validation :set_material
   before_validation :set_user
 
-
   validates :iso2709, presence: true
   validates :iso2709, uniqueness: true
 
@@ -14,8 +13,8 @@ class BiblioRecord < ApplicationRecord
     self.material = 'book'
   end
 
-
-  # Ultima criacao e edicao serao do usuario 1
+  # Ultima criacao e edicao serao do usuario 1,
+  # Que é o admin default do biblivre
   def set_user
     self.created_by = 1
     self.modified_by = 1
