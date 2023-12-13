c37881c211ad42fd9d0d956e4eed49922343032c
Gerson Azevedo <gersonazgo@gmail.com>
2023-12-13 13:25:13 -0300

    Store more changes on holdings generator service

---
+++ app/services/marc/biblio_holding/generator_service.rb
@@ -0,0 +1,81 @@
+# Responsável por gerar um MARC::Record a partir de um Csv::Holding
+# válido para um registro de exemplar (biblio_holding) e uma
+# id de registro de bibliográfico (biblio_record)
+class Marc::BiblioHolding::GeneratorService
+
+  def initialize(csv_holding:, record_id:)
+    @csv_holding = csv_holding
+    @record_id = record_id
+  end
+
+  def call
+    generate_biblio_holding_marc
+  end
+
+  private
+
+  # def generate_biblio_holding_marc
+  #   raise 'Holding inválido' unless @csv_holding.valid?
+
+  #   record = MARC::Record.new
+
+  #   # Campo 001: Número de controle (Número de Tombo)
+  #   # ATENCAO: existe uma redundancia entre este campo MARC e o accession_number
+  #   # no model. Eu suponho que é para otimizar busca
+  #   record.append(MARC::ControlField.new('001', @csv_holding.nro_tombo.rjust(5, '0')))
+
+  #   # Campo 003: Identificador do número de controle
+  #   record.append(MARC::ControlField.new('003', '1'))
+
+  #   # Campo 005: Data e hora da última transação
+  #   record.append(MARC::ControlField.new('005', DateTime.now.strftime('%Y%m%d%H%M%S.%L')))
+
+  #   # Campo 100: Autor
+  #   record.append(MARC::DataField.new('100', '1', ' ', ['a', @csv_holding.autor]))
+
+  #   # Campo 245: Título
+  #   record.append(MARC::DataField.new('245', '1', '0', ['a', @csv_holding.titulo]))
+
+  #   # Campo 260: Ano de Publicação
+  #   # Ajuste os subcampos conforme necessário
+  #   record.append(MARC::DataField.new('260', ' ', ' ', ['c', @csv_holding.ano]))
+
+  #   # Campo 852: Localização da Chamada
+  #   # # ATENCAO: existe uma redundancia entre este campo MARC e o location_d
+  #   # no model. Eu suponho que é para otimizar busca
+  #   record.append(MARC::DataField.new('852', ' ', ' ', ['a', @csv_holding.local_chamada]))
+
+  #   record.to_marc
+  # end
+
+  def generate_biblio_holding_marc
+    raise 'Holding inválido' unless @csv_holding.valid?
+
+    record = MARC::Record.new
+
+    # Campo 000: Cabeçalho do Registro
+    record.append(MARC::ControlField.new('000', '00170nu a2200097un 4500'))
+
+    # Campo 001: Número de controle (Número de Tombo)
+    # record.append(MARC::ControlField.new('001', @csv_holding.nro_tombo.rjust(7, '0')))
+
+    # Campo 004: Identificador do número de controle
+    record.append(MARC::ControlField.new('004', "#{@record_id}"))
+
+    # Campo 005: Data e hora da última transação
+    record.append(MARC::ControlField.new('005', DateTime.now.strftime('%Y%m%d%H%M%S.%L')))
+
+    # Campo 090: Código de Localização
+    # Subcampo 'a' para a localização e 'e' para o exemplar
+    record.append(MARC::DataField.new('090', ' ', ' ', ['a', "#{@csv_holding.local_chamada}"], ['e', 'ex.1']))
+
+    # Campo 541: Informações de Aquisição (pode ser ajustado conforme necessário)
+    record.append(MARC::DataField.new('541', ' ', ' '))
+
+    # Campo 949: Informações Adicionais (pode ser ajustado conforme necessário)
+    record.append(MARC::DataField.new('949', ' ', ' ', ['a', "#{@csv_holding.nro_tombo}"]))
+
+    record.to_marc
+  end
+
+end
--- spec/services/marc/biblio_record/generator_service_spec.rb
+++ spec/services/marc/biblio_record/generator_service_spec.rb
@@ -3,12 +3,13 @@ require 'rails_helper'
 RSpec.describe Marc::BiblioRecord::GeneratorService, type: :service do
   before do
     @csv_holding = FactoryBot.build(:csv_holding)
+    @biblio_record = FactoryBot.build(:biblio_record)
   end
 
   describe '#call' do
     context 'when csv_holding is valid' do
       it 'returns a MARC string' do
-        service = described_class.new(csv_holding: @csv_holding)
+        service = described_class.new(csv_holding: @csv_holding, biblio_record: @biblio_record)
         expect(service.call).to be_a(String)
       end
     end
@@ -16,7 +17,7 @@ RSpec.describe Marc::BiblioRecord::GeneratorService, type: :service do
     context 'when csv_holding is invalid' do
       it 'raises an error' do
         @csv_holding.nro_tombo = nil
-        service = described_class.new(csv_holding: @csv_holding)
+        service = described_class.new(csv_holding: @csv_holding, biblio_record: @biblio_record)
         expect { service.call }.to raise_error(RuntimeError, 'Holding inválido')
       end
     end
