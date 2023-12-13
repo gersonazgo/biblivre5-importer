9a928e7ad710647cca77144e09e5de38b5c5a73a
Gerson Azevedo <gersonazgo@gmail.com>
2023-12-13 13:23:51 -0300

    Many improments to biblio record generator service

--- app/services/marc/biblio_record/generator_service.rb
+++ app/services/marc/biblio_record/generator_service.rb
@@ -2,8 +2,9 @@
 # válido para um registro de bibliográfico (biblio_record)
 class Marc::BiblioRecord::GeneratorService
 
-  def initialize(csv_holding:)
+  def initialize(csv_holding:, biblio_record:)
     @csv_holding = csv_holding
+    @biblio_record = biblio_record
   end
 
   def call
@@ -17,26 +18,39 @@ class Marc::BiblioRecord::GeneratorService
 
     record = MARC::Record.new
 
-    # 2. Chamada/Localização - Campo de Controle (001)
-    record.append(MARC::ControlField.new('001', @csv_holding.local_chamada))
+    # Supondo que @biblio_record possui um campo de ID único que possa ser usado aqui
+    # Ajustar este campo conforme necessário
+    record_id = @biblio_record.id || '0000000'
+    record.append(MARC::ControlField.new('001', record_id.to_s.rjust(7, '0')))
 
-    # 3. Autor - Campo de Dados (100)
+    record.append(MARC::ControlField.new('005', Time.now.strftime('%Y%m%d%H%M%S.%L')))
+
+    # Supondo que todos os registros são do tipo 'cam' para o campo '008'
+    # Isso pode precisar de ajuste conforme o tipo de material
+    record.append(MARC::ControlField.new('008', '231106s|||| bl|||||||||||||||||por|u'))
+
+    # Campo 090: Código de Localização
+    record.append(MARC::DataField.new('090', ' ', ' ', ['a', @csv_holding.local_chamada]))
+
+    # Campo 100: Autor Principal
     record.append(MARC::DataField.new('100', '1', ' ', ['a', @csv_holding.autor]))
 
-    # 4. Título - Campo de Dados (245)
+    # Campo 245: Título
     record.append(MARC::DataField.new('245', '1', '0', ['a', @csv_holding.titulo]))
 
-    # 5. Local (Cidade) - Subcampo do Campo de Dados (260 $a)
-    # 7. Editora - Subcampo do Campo de Dados (260 $b)
-    # 8. Ano - Subcampo do Campo de Dados (260 $c)
+    # Campo 260: Ano de Publicação, Cidade e Editora
     record.append(MARC::DataField.new('260', ' ', ' ', ['a', @csv_holding.local_cidade], ['b', @csv_holding.editora], ['c', @csv_holding.ano]))
 
-    # 6. Estado/País - Campo de Dados (264 $a)
-    record.append(MARC::DataField.new('264', ' ', ' ', ['a', @csv_holding.est_pais]))
-
-    # 9. Observações - Campo de Dados (500)
+    # Campo 500: Notas Gerais (Observações)
     record.append(MARC::DataField.new('500', ' ', ' ', ['a', @csv_holding.obs]))
+
+    temp_leader = ' ' * 24
+    record.leader = temp_leader
+    record_length = record.to_marc.length.to_s.rjust(5, '0')
+    material_code = 'cam'
+    record.leader = "#{record_length}#{material_code} a2200109 a 4500"
 
     record.to_marc
   end
+
 end
